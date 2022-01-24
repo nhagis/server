78b8e57803c8392b909ff266275eb950bab4f2cd
nhagis <nhagispc2@gmail.com>
2022-01-24 12:56:40 +0500

    24/1/2022

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
+// const devConfiq = {
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   port: process.env.PG_PORT,
+// };
+
+// const pool = new Pool(devConfiq);
+
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "perntodo",
-  password: "admin",
-  port: 5432,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
-let port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -70,6 +70,6 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log("server has started on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
 });
